f5820d2f7c13f87fe0f93583f9607f1ebaefd50e
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-06-07 06:46:16 +0200

    deploying to heroku

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const useRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin/auth");
 const cors = require('cors');
+const path = require("path");
 // import router from './routes/user';
 
 //initializing environment variables
@@ -20,6 +21,18 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', useRoutes);
 app.use('/api', adminRoutes);
+
+// Serve the frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      )
+    );
+  } else {
+    app.get("/", (req, res) => res.send("Please set to production"));
+  }
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
