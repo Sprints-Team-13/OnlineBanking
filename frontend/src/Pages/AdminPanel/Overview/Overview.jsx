bd619f36e7f3f91f5f2d6577a54aa6ba2420fe38
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-06-16 21:17:00 +0200

    add recent transactions to admin overview

--- frontend/src/Pages/AdminPanel/Overview/Overview.jsx
+++ frontend/src/Pages/AdminPanel/Overview/Overview.jsx
@@ -1,5 +1,7 @@
 import "./overview.scss"
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+
 import useGetUsers from '../../../hooks/queries/admin/useGetUsers'
 import useGetAccounts from '../../../hooks/queries/admin/useGetAccounts'
 import useGetTransactions from '../../../hooks/queries/admin/useGetTransactions'
@@ -11,6 +13,44 @@ function Overview() {
   const {data: users} = useGetUsers()
   const {data: accounts} = useGetAccounts()
   const {data: transactions} = useGetTransactions()
+
+  // convert date to string
+  function date(date) {
+    const display = new Date(date)
+    return display.toLocaleDateString('en-GB');
+  }
+
+  const columns = [
+    { 
+      field: 'date', headerName: 'Date', type: 'date' , minWidth: 100, flex: 1
+    },
+    { 
+      field: 'accountNumber', headerName: 'Account Number', minWidth: 130, flex: 1
+    },
+    { 
+      field: 'transactionType', headerName: 'Type', minWidth: 70, flex: 1
+    },
+    { 
+      field: 'amount', headerName: 'Amount', minWidth: 70, flex: 1
+    },
+    { 
+      field: 'description', headerName: 'Description', minWidth: 130, flex: 2
+    },
+    { 
+      field: 'id', headerName: 'Transaction ID', minWidth: 150, flex: 2.2
+    },
+  ];
+
+  const rows = transactions && transactions.slice(0, 5).map(transaction => (
+    {
+      date: date(transaction.transactionDate),
+      id: transaction._id,
+      transactionType: transaction.transactionType,
+      accountNumber: transaction.accountNumber,
+      amount: `$${transaction.amount}`,
+      description: transaction.description,
+    }
+  ))
 
   return (
     <div className="overview">
@@ -34,6 +74,35 @@ function Overview() {
 
         <div className="right-section-wrapper">
           <div className="right-section">
+
+            <h3>Recent Transactions</h3>
+
+            <div style={{ height: 400, width: '90%' }}>
+              <div style={{ display: 'flex', height: '100%' }}>
+                <div className="table-container">
+                  {transactions &&
+                  <DataGrid
+                    autoHeight
+                    className='table'
+                    rows={rows}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    disableSelectionOnClick
+                    disableColumnMenu
+                    hideFooterPagination
+                    hideFooter
+                    sx={{
+                      '& .MuiDataGrid-cell:hover': {
+                        cursor: 'pointer'
+                      },
+                    }}
+                  />
+                  }
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
 
