eb51686435409a5afdd0ac5c01cc950e653b8039
AhmedBasem <demhamesab@gmail.com>
2022-06-16 15:16:38 +0200

    fix typo

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -4,7 +4,7 @@ const { getID } = require("./user")
 //send transactions to user
 exports.getTransactions = (req, res) => {
     let userID = getID(req)
-    Transaction.find({cunstomerID: userID}).
+    Transaction.find({customerID: userID}).
     exec(async (error, transactions) => {
         if (error) return res.status(400).json({ error })
         res.status(200).json(transactions)
