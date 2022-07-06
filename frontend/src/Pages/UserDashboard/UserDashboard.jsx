14e4d8af12de7f4566dbfb5d7cdb06ee3b7a8128
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-07-06 22:58:32 +0200

    fix user & admin pages on small screens

--- frontend/src/Pages/UserDashboard/UserDashboard.jsx
+++ frontend/src/Pages/UserDashboard/UserDashboard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./userdashboard.scss"
 import {Routes, Route} from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
 
 import Sidebar from '../../layout/sidebar/UserSidebar'
 import Navbar from '../../layout/navbar/Navbar'
@@ -11,15 +12,34 @@ import Transfer from './Transfer/Transfer'
 import Profile from './Profile/Profile'
 
 function UserDashboard() {
+
+  // control the responsive sidebar
+  const [isSidebarActive, setIsSidebarActive] = useState(false)
+
+  // toggle sidebar
+  function toggleSidebar() {
+    setIsSidebarActive(prev => !prev)
+  }
+
+  // declare media queries
+  const isSmallScreen = useMediaQuery({query: '(max-width: 600px)'})
+
+  // auto detect media queries
+  useEffect(()=> {
+    if(isSmallScreen) {
+      setIsSidebarActive(true)
+    }
+  },[])
+
   return (
     <div className='userdashboard'>
       <img src='https://img.freepik.com/free-photo/white-concrete-wall_53876-92803.jpg?w=1060&t=st=1654925214~exp=1654925814~hmac=a1af552ae8c89191873bfa8091490cf7f34e2deb6d58228fa761e10577ce6ade' alt='bg' className='bg'></img>
       
-      <Sidebar />
-      <div className="userdashboard-container">
+      <Sidebar  isSidebarActive={isSidebarActive} toggleSidebar={toggleSidebar} />
+      <div className={isSidebarActive ? 'userdashboard-container collapse' : 'userdashboard-container'}>
 
         <header>
-          <Navbar />
+          <Navbar isSidebarActive={isSidebarActive}/>
         </header>
 
         <main className="userdashboard-main">
--- frontend/src/layout/sidebar/UserSidebar.jsx
+++ frontend/src/layout/sidebar/UserSidebar.jsx
@@ -11,20 +11,27 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 import { AuthContext } from '../../context/Auth-context';
 
-function Sidebar() {
+function Sidebar(props) {
 
+  // import signout fnc form auth context
   const { signOut } = useContext(AuthContext)
 
-  return (
-    <div className='sidebar'>
+    // destracture props
+    const {isSidebarActive, toggleSidebar} = props
 
-      <div className="top">
+  return (
+    <div className={isSidebarActive ? 'sidebar collapse' : 'sidebar'}>
+
+      <div className="top" onClick={toggleSidebar}>
+        
         <img src='https://api.iconify.design/bxs/bank.svg?color=white' alt='LOGO'></img>
-        <span className="logo">
-          <Link to={'/'}>
+
+        {!isSidebarActive &&
+          <p className="logo">
             Sprints Bank
-          </Link>
-        </span>
+          </p>
+        }
+
       </div>
       
       <div className="center">
@@ -33,48 +40,48 @@ function Sidebar() {
         <Link to={'/userdashboard'}>
           <li>
             <DashboardIcon className='icon'/>
-            <span>Overview</span>
+            {!isSidebarActive && <p>Overview</p>} 
           </li>
         </Link>
 
         <Link to={'/userdashboard/accounts'}>
           <li>
             <AccountBalanceWalletIcon className='icon'/>
-            <span>Accounts</span>
+            {!isSidebarActive && <p>Accounts</p>}
           </li>
         </Link>
 
         <Link to={'/userdashboard/transfer'}>
           <li>
             <TransformOutlinedIcon className='icon'/>
-            <span>Money Transfer</span>
+            {!isSidebarActive && <p>Money Transfer</p>}
           </li>
         </Link>
 
         <Link to={'/userdashboard/transactions'}>
           <li>
             <ViewListOutlinedIcon className='icon'/>
-            <span>Transactions</span>
+            {!isSidebarActive && <p>Transactions</p>}
           </li>
         </Link>
 
         <Link to={'/userdashboard/profile'}>
           <li>
             <AccountCircleIcon className='icon'/>
-            <span>Profile</span>
+            {!isSidebarActive && <p>Profile</p>}
           </li>
         </Link>
 
         <Link to={'/userdashboard/settings'}>
           <li>
             <SettingsIcon className='icon'/>
-            <span>Settings</span>
+            {!isSidebarActive && <p>Settings</p>}
           </li>
         </Link>
         
           <li onClick={signOut}>
             <ExitToAppOutlinedIcon className='icon'/>
-            <span>Signout</span>
+            {!isSidebarActive && <p>Signout</p>}
           </li>   
 
         </ul>
