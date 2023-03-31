b0b65188b6ffca7323126601f4273b2d9f85dbd9
Ali Joudeh <ali.joudeh@holocene.eu>
2023-04-01 01:58:15 +0300

    updated transfer screen

--- frontend/src/schemas/transferSchema.js
+++ frontend/src/schemas/transferSchema.js
@@ -7,9 +7,10 @@ export const transferSchema = yup.object({
     .required('Account number is required'),
   amount: yup.number()
     .positive('Enter a vaild amount')
+    .moreThan(99, 'Minimum amount is 100')
     .required('Amount is required'),
   destinationAccountNumber: yup.string()
-    .min(10, 'Minimum  characters allowed are 11')
+    .min(6, 'Minimum  characters allowed are 11')
     .max(10, 'Maximum characters allowed are 11')
     .required('Account number is required'),
 })
